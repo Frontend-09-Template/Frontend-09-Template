fdd08b10e7b29c6bd2a38358b737a52a178c2818
chengyifan1 <chengyifan1@360.cn>
2021-06-03 17:29:17 +0800

    feat: update

---
+++ Week_14/jsx/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/***/ (() => {
+
+eval("function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== \"undefined\" && o[Symbol.iterator] || o[\"@@iterator\"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === \"number\") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError(\"Invalid attempt to iterate non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\"); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it[\"return\"] != null) it[\"return\"](); } finally { if (didErr) throw err; } } }; }\n\nfunction _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === \"string\") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === \"Object\" && o.constructor) n = o.constructor.name; if (n === \"Map\" || n === \"Set\") return Array.from(o); if (n === \"Arguments\" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }\n\nfunction _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }\n\nfunction createElement(type, attribute) {\n  var element = document.createElement(type);\n\n  for (var name in attribute) {\n    element.setAttribute(name, attribute[name]);\n  }\n\n  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    children[_key - 2] = arguments[_key];\n  }\n\n  if (children.length > 0) {\n    var _iterator = _createForOfIteratorHelper(children),\n        _step;\n\n    try {\n      for (_iterator.s(); !(_step = _iterator.n()).done;) {\n        var child = _step.value;\n        element.appendChild(child);\n      }\n    } catch (err) {\n      _iterator.e(err);\n    } finally {\n      _iterator.f();\n    }\n  }\n\n  return element;\n}\n\nvar arr = [1, 2, 3];\n\nfor (var _i = 0, _arr = arr; _i < _arr.length; _i++) {\n  var i = _arr[_i];\n  console.log(i);\n}\n\nvar a = createElement(\"div\", {\n  id: \"a\"\n}, createElement(\"span\", null), createElement(\"span\", null), createElement(\"span\", null)); // var a = createElement(\"div\", {\n//   id: \"a\",\n// }, \n// createElement(\"span\", null, \"a\"),    // 子元素递归的调用createElement，其实形成了一个树形的结构，后面的子元素作为参数传递进来\n// createElement(\"span\", null, \"b\"),\n// createElement(\"span\", null, \"c\"));\n\ndocument.body.appendChild(a);\n\n//# sourceURL=webpack://jsx/./main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./main.js"]();
+/******/ 	
+/******/ })()
+;
--- Week_14/jsx/main.js
+++ Week_14/jsx/main.js
@@ -1,5 +1,32 @@
+function createElement(type, attribute, ...children) {
+  let element = document.createElement(type);
+  for (let name in attribute) {
+    element.setAttribute(name, attribute[name]);
+  }
+  if (children.length > 0) {
+    for (let child of children) {
+      element.appendChild(child);
+    }
+  }
+  return element;
+}
+
 const arr = [1, 2, 3];
 for (let i of arr) {
   console.log(i);
 }
-let a = <div></div>
+let a = <div id="a">
+  <span></span>
+  <span></span>
+  <span></span>
+</div>;
+
+
+// var a = createElement("div", {
+//   id: "a",
+// }, 
+// createElement("span", null, "a"),    // 子元素递归的调用createElement，其实形成了一个树形的结构，后面的子元素作为参数传递进来
+// createElement("span", null, "b"),
+// createElement("span", null, "c"));
+
+document.body.appendChild(a);
