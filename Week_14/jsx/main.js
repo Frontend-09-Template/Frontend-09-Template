eb97c4f1210e57d91f5705f1fda7070c4aafcdb7
chengyifan1 <chengyifan1@360.cn>
2021-06-04 18:16:33 +0800

    feat: update week_14

--- Week_14/jsx/main.js
+++ Week_14/jsx/main.js
@@ -1,25 +1,82 @@
 function createElement(type, attribute, ...children) {
-  let element = document.createElement(type);
+  let element;
+  if (typeof type === "string") {
+    element = document.createElement(type);
+  } else {
+    element = new type;
+  }
+  
   for (let name in attribute) {
     element.setAttribute(name, attribute[name]);
   }
   if (children.length > 0) {
     for (let child of children) {
+      if (typeof child === "string") {
+        child = document.createTextNode(child);
+      }
       element.appendChild(child);
     }
   }
   return element;
 }
 
+
+class ElementWrapper {
+  constructor(type) {
+    this.root = document.createElement(type);
+  }
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+
+  appendChild(child) {
+    this.root.appendChild(child);
+  }
+  
+  mountTo(parent) {
+    parent.appendChild(this.root)
+  }
+}
+
+
+class Div {
+  constructor() {
+    this.root = document.createElement("div");
+  }
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+
+  appendChild(child) {
+    this.root.appendChild(child);
+  }
+
+  mountTo(parent) {
+    parent.appendChild(this.root)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const arr = [1, 2, 3];
 for (let i of arr) {
   console.log(i);
 }
-let a = <div id="a">
-  <span></span>
-  <span></span>
-  <span></span>
-</div>;
+let a = <Div id="a">
+  <span>e</span>
+  <span>f</span>
+  <span>g</span>
+</Div>;
 
 
 // var a = createElement("div", {
@@ -29,4 +86,6 @@ let a = <div id="a">
 // createElement("span", null, "b"),
 // createElement("span", null, "c"));
 
-document.body.appendChild(a);
+// document.body.appendChild(a);
+
+a.mountTo(document.body);
